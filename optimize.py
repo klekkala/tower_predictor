6f45ca957ebf26d035b15f1d976c211c79cc7528
kiran4399 <kiran4399@gmail.com>
2016-10-31 19:23:42 +0530

    lm algo with qr decomposotion done

--- optimize.py
+++ optimize.py
@@ -1,10 +1,12 @@
-import numpy as np
-from scipy.optimize import minimize
-
+#!/usr/bin/python
+#-*- coding: latin-1 -*-
+"""This module contains pure Python implementations of the
+Levenberg-Marquardt algorithm for data fitting.
+"""
 
 import numpy
 
-from numpy import inner, max, diag, eye, Inf, dot
+from numpy import inner, diag, eye, Inf, dot
 from numpy.linalg import norm, solve
 
 
@@ -105,7 +107,7 @@ def fitparerror(fitpar, J, res):
   
     alpha = 0.05 #confidence level 95% for alpha = 0.05, 2sigma confidence limit
     
-    N = max(J.shape) #number of points
+    N = numpy.max(J.shape) #number of points
     m = len(fitpar) #number of parameters
 
     rnorm = sum(res*res) #norm residuum
@@ -123,75 +125,6 @@ def fitparerror(fitpar, J, res):
 
     return parerr, sigma
 
-    
-def LM(fun, pars, args,
-       tau = 1e-2, eps1 = 1e-6, eps2 = 1e-6, kmax = 20,
-       verbose = False,
-       full_output = False):
-    """Implementation of the Levenberg-Marquardt algorithm in pure
-    Python. Solves the normal equations."""
-    p = pars
-    f, J = fun(p, *args)
-
-    A = inner(J,J)
-    g = inner(J,f)
-
-    I = eye(len(p))
-
-    k = 0; nu = 2
-    mu = tau * max(diag(A))
-    stop = norm(g, Inf) < eps1
-    while not stop and k < kmax:
-        k += 1
-
-        try:
-            d = solve( A + mu*I, -g)
-        except numpy.linalg.LinAlgError:
-            print "Singular matrix encountered in LM"
-            stop = True
-            reason = 'singular matrix'
-            break
-
-        if norm(d) < eps2*(norm(p) + eps2):
-            stop = True
-            reason = 'small step'
-            break
-
-        pnew = p + d
-
-        fnew, Jnew = fun(pnew, *args)
-        #rho = (norm(f) - norm(fnew))/inner(d, mu*d - g)  # /2????
-        rho = (norm(f)**2 - norm(fnew)**2)/inner(d, mu*d - g)
-        
-        if rho > 0:
-            p = pnew
-            A = inner(Jnew, Jnew)
-            g = inner(Jnew, fnew)
-            f = fnew
-            J = Jnew
-            if (norm(g, Inf) < eps1): # or norm(fnew) < eps3):
-                stop = True
-                reason = "small gradient"
-                break
-            mu = mu * max([1.0/3, 1.0 - (2*rho - 1)**3])
-            nu = 2.0
-        else:
-            mu = mu * nu
-            nu = 2*nu
-
-        if verbose:
-            print "step %2d: |f|: %9.6g mu: %8.3g rho: %8.3g"%(k, norm(f), mu, rho)
-
-    else:
-        reason = "max iter reached"
-
-    if verbose:
-        print reason
-    
-    if not full_output:
-        return p
-    else:
-        return p, J, f #fitparerror(p, J, f)
 
 def LMqr(fun, pars, args,
          tau = 1e-3, eps1 = 1e-8, eps2 = 1e-8, kmax = 100,
@@ -213,7 +146,7 @@ def LMqr(fun, pars, args,
     I = eye(len(p))
 
     k = 0; nu = 2
-    mu = tau * max(diag(A))
+    mu = tau * numpy.max(diag(A))
     stop = norm(g, Inf) < eps1
 
     while not stop and k < kmax:
@@ -242,7 +175,7 @@ def LMqr(fun, pars, args,
         tic = time.time()
         #q, r = scipy.linalg.qr(Des, econ = True, mode = 'qr')
         #d4   = solve(r, inner(numpy.transpose(q), des))
-        r = scipy.linalg.qr(Des, econ = True, mode = 'r')
+        q, r = scipy.linalg.qr(Des, mode = 'economic')
         d4   = scipy.linalg.cho_solve( (r, False), -inner(J, f))
         print 'd4', d4, time.time() - tic
 
@@ -250,7 +183,7 @@ def LMqr(fun, pars, args,
         
 
         tic = time.time()
-        q, r = scipy.linalg.qr(numpy.transpose(J), econ = True, mode = 'qr')
+        q, r = scipy.linalg.qr(numpy.transpose(J), mode = 'economic')
         d3 = solve( r + mu*numpy.linalg.inv(r.transpose()), -inner(numpy.transpose(q),f))
         #d3 = scipy.linalg.cho_solve( (r + mu*numpy.linalg.inv(r.transpose()), False),
         #                             -inner(numpy.transpose(q),f))
@@ -281,7 +214,9 @@ def LMqr(fun, pars, args,
                 stop = True
                 reason = "small gradient"
                 break
-            mu = mu * max(1.0/3, 1 - (2*rho - 1)**3)
+            print rho
+            mu = mu*max(1.0/3, 1 - (2*rho - 1)**3)
+            print "end"
             nu = 2
         else:
             mu = mu * nu
@@ -294,19 +229,6 @@ def LMqr(fun, pars, args,
         print reason
     return p
 
-
-def testLM():
-    #       A   m   s  offs
-    pars = [1, 0.1, 1, 0.5]
-    
-    x = numpy.linspace(-3,3,1001)
-    y = gauss1d(pars, x)
-
-    #y+= numpy.random.randn(len(x))
-    #            A   m    s  offs
-    startpars = [1*2, 0.5, 2, 0.5]
-
-    return LM(fJ, startpars, (x, y), verbose = True, tau = 1e-4)
 
 def testfLMs():
     x = numpy.linspace(-3,3,1001)
@@ -339,6 +261,14 @@ def testLMqr():
     return LMqr(fJ, pars2, (x, y), verbose = True)
 
 
+
+if __name__ == '__main__':
+    print testLMqr()
+    #print '-'*40
+    #print testLMqr()
+    
+
+
 def cost(x):
 
 	cost_value = 0
@@ -360,4 +290,3 @@ def minimize(priority):
   x0 = np.array([0.0, 0.0])
   res = minimize(cost, x0, method='lm', options={'xtol': 1e-8, 'disp': True})
   return res.x
-
